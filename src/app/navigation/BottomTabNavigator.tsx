a1426e429f9e74102d45e68600aa9883af9abb90
Saurav799-bit <152502019@smail.iitpkd.ac.in>
2026-05-11 15:39:57 +0530

    Added pod for iOS and android gradle configs

--- src/app/navigation/BottomTabNavigator.tsx
+++ src/app/navigation/BottomTabNavigator.tsx
@@ -15,16 +15,8 @@ const BottomTabNavigator = () => {
       screenOptions={({ route }) => ({
         headerShown: false,
 
-        tabBarIcon: ({ color, size }) => {
-          let iconName = '';
-
-          if (route.name === 'Home') {
-            iconName = 'home-outline';
-          } else if (route.name === 'Cart') {
-            iconName = 'cart-outline';
-          } else if (route.name === 'Profile') {
-            iconName = 'person-outline';
-          }
+        tabBarIcon: ({ color, size, focused }) => {
+          const iconName = getIconName(route.name, focused)
 
           return (
             <Ionicons
@@ -37,10 +29,26 @@ const BottomTabNavigator = () => {
       })}
     >
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Categories" component={HomeScreen} />
       <Tab.Screen name="Cart" component={CartScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 };
+
+const getIconName = (routeName: String, focused: boolean) => {
+  switch(routeName) {
+    case 'Home':
+      return focused ? 'home' : 'home-outline';
+    case 'Categories':
+      return focused ? 'grid' : 'grid-outline';
+    case 'Cart':
+      return focused ? 'cart' : 'cart-outline';
+    case 'Profile':
+      return focused ? 'person' : 'person-outline';
+    default:
+      return 'help-circle';
+  }
+}
 
 export default BottomTabNavigator;
